50382e61c2bf6b3a819a8b6cfb56d869cdcf9d00
maxim <vyatkin.maxim@gmail.com>
2021-03-21 22:48:44 +0300

    hw4

---
+++ lesson4/homework4.sql
@@ -0,0 +1,6 @@
+USE vk;
+
+UPDATE media_types SET name = 'photo' WHERE id = 1;
+UPDATE media_types SET name = 'video' WHERE id = 2;
+UPDATE media_types SET name = 'document' WHERE id = 3;
+UPDATE media_types SET name = 'gif' WHERE id =4;
